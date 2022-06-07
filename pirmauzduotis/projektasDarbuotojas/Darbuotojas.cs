c86a533ad2ec5407d911ff240ff6b0ad6dff3b67
Znamenskas <zvilandas@gmail.com>
2022-06-07 09:42:24 +0300

    Objekto-Darbuotojas-nuskaitymas-is-konsoles

---
+++ pirmauzduotis/projektasDarbuotojas/Darbuotojas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace projektasDarbuotojas
+{
+    public class Darbuotojas
+    {
+
+        // 1.Klases kintamieji
+        private string vardas;
+        private string pavarde;
+        private int amzius;
+        private string lytis;
+        private string profesija;
+        private double atlyginimas;
+        private int darboValandos;
+        private double valandosIkainis;
+
+        // Konstruktoriai
+
+        public Darbuotojas(string vardas, string pavarde, int amzius, string lytis, string profesija, double atlyginimas, int darboValandos, double valandosIkainis)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.amzius = amzius;
+            this.lytis = lytis;
+            this.profesija = profesija;
+            this.atlyginimas = atlyginimas;
+            this.darboValandos = darboValandos;
+            this.valandosIkainis = valandosIkainis;
+        }
+        // Tuscias KONSTRUKTORIUS
+
+        public Darbuotojas()
+        {
+
+        }
+
+        // GALI BUTI DAR KONSTRUKTORIU JEIGU JU REIKIA.
+
+        // GETERIAI SETERIAI.
+
+        public string Vardas
+        {
+            get { return vardas; }
+            set { vardas = value; }
+        }
+        public string Pavarde
+        {
+            get { return pavarde; }
+            set { pavarde = value; }
+        }
+        public int Amzius
+        {
+            get { return amzius; }
+            set { amzius = value; }
+        }
+        public string Lytis
+        {
+            get { return lytis; }
+            set { lytis = value; }
+        }
+        public string Profesija
+        {
+            get { return profesija; }
+            set { profesija = value; }
+        }
+        public double Atlyginimas
+        {
+            get { return atlyginimas; }
+            set { atlyginimas = value; }
+        }
+        public int DarboValandos
+        {
+            get { return darboValandos; }
+            set { darboValandos = value; }
+        }
+        public double ValandosIkainis
+        {
+            get { return valandosIkainis; }
+            set { valandosIkainis = value; }
+        }
+
+    }
+
+}
--- pirmauzduotis/projektasDarbuotojas/Program.cs
+++ pirmauzduotis/projektasDarbuotojas/Program.cs
@@ -2,11 +2,85 @@
 
 namespace projektasDarbuotojas
 {
-    class Program
+    internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // susikuriu kaip KINTAMAJI
+            Darbuotojas darbuotojas1;
+
+            darbuotojas1 = NuskaitytiDarbuotojas();
+
+            IsvestiDarbuotoja(darbuotojas1);
+
+            // SUSIKURIU PAPYLDOMA FUNKCIJA IR NUSKAITYTI VIENA DARBUOTOJO OBJEKTA IR JI GRAZINTI.
+        static Darbuotojas NuskaitytiDarbuotojas()
+            {
+                Darbuotojas darbuotojas = new Darbuotojas();
+                Console.WriteLine("Ivesti Darbuotoja: ");
+                string pavadinimas = Console.ReadLine();
+                darbuotojas.Vardas = pavadinimas;
+
+                return darbuotojas;
+            }
+
+        static void IsvestiDarbuotoja(Darbuotojas darbuotojas)
+            {
+                Console.WriteLine("Vardas: " + darbuotojas.Vardas);
+                Console.WriteLine("Pavarde: " + darbuotojas.Pavarde);
+                Console.WriteLine("Amzius: " + darbuotojas.Amzius);
+                Console.WriteLine("Lytis: " + darbuotojas.Lytis);
+                Console.WriteLine("Profesija: " + darbuotojas.Profesija);
+                Console.WriteLine("Atlyginimas: " + darbuotojas.Atlyginimas);
+                Console.WriteLine("Darbo valandos: " + darbuotojas.DarboValandos);
+                Console.WriteLine("Valandos ikainis: " + darbuotojas.ValandosIkainis);
+
+                Console.WriteLine("_ _ _ _ _ _ _ _ _ _  _ _ _ _ _ _ _ _");
+            }    
+
+
+            // sukuriu tuscia objekta Darbuotojas
+            Darbuotojas darbuotojas = new Darbuotojas();
+
+            // Bandau isvesti i konsole.
+            //Console.WriteLine(darbuotojas.ToString());
+
+            // Sukuriu pilna objekta Darbuotojas
+
+            Darbuotojas darbuotojas2 = new Darbuotojas("Vilandas", "Znamenskas", 54, "Vyras", "Programuotojas", 1920, 160, 12);
+
+            // Isvedu pilna objekta i Darbuotojas i konsole.
+           // Console.WriteLine(darbuotojas2.ToString());
+
+            // Gaunu po viena objekto Darbuotojas duomeni ir ji isvedu i konsole.
+
+            Console.WriteLine("Vardas:" + darbuotojas2.Vardas);
+            Console.WriteLine("Pavarde:" + darbuotojas2.Pavarde);
+            Console.WriteLine("Amzius:" + darbuotojas2.Amzius);
+            Console.WriteLine("Lytis:" + darbuotojas2.Lytis);
+            Console.WriteLine("Profesija:" + darbuotojas2.Profesija);
+            Console.WriteLine("Atlyginimas:" + darbuotojas2.Atlyginimas);
+            Console.WriteLine("Darbo valandos:" + darbuotojas2.DarboValandos);
+            Console.WriteLine("Valandos ikainis:" + darbuotojas2.ValandosIkainis);
         }
+        
+
+
+
+
+
+
+
+
+
+
     }
 }
+        
+
+    
+
+
+
